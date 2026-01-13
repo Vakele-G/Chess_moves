1a28ab04b45e980d97af874009f3b9ebf95d9e45
Vakele <vakelegawd@gmail.com>
2026-01-13 08:41:39 +0200

    Remove sys.arg

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import sys
 from game_functions import generate_board, print_board, game_state, white_pawn_moves, black_pawn_moves, white_rook_moves, black_rook_moves, white_knight_moves, black_knight_moves, white_bishop_moves, black_bishop_moves
 from game_functions import Game
 
-fen_string =  sys.argv[1]
+
 '''
 board = generate_board(fen_string)  # create 2d array of chess board
 
@@ -19,7 +19,16 @@ print(f"White bishop moves: {white_bishop_moves(board)}")
 print(f"Black bishop moves: {black_bishop_moves(board)}")
 '''
 
-new_game = Game(fen_string)
+print("/////////////////////////////////////////////////")
+print("Type 'new game' to begin a fresh game as white OR")
+fen_string = input("enter a FEN string to play from that position: ").strip().lower()
+
+if fen_string == "new game":
+    new_game = Game("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+else:
+    new_game = Game(fen_string)
+
+print()
 print_board(new_game.board)
 
 while True:
@@ -29,3 +38,6 @@ while True:
 
     print_board(new_game.board)
     print()
+
+
+"""Right now, illegal moves are able to be played. Checks, checkmate, enpassant and promotion are not implemented"""
