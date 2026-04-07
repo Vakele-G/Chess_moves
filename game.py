eed97c4e14a705af17dfa60ff0ce9d9d80f11a0d
vagodjhb025 <vagodjhb025@student.wethinkcode.co.za>
2026-04-07 15:00:36 +0200

    Test commit

--- game.py
+++ game.py
@@ -2,22 +2,6 @@ import sys
 from game_functions import *
 
 
-'''
-board = generate_board(fen_string)  # create 2d array of chess board
-
-print_board(board)
-
-game_state(fen_string)  # print state of game details
-print(f"White pawn moves: {white_pawn_moves(board)}")
-print(f"Black pawn moves: {black_pawn_moves(board)}")
-print(f"White rook moves: {white_rook_moves(board)}")
-print(f"Black rook moves: {black_rook_moves(board)}")
-print(f"White knight moves: {white_knight_moves(board)}")
-print(f"Black knight moves: {black_knight_moves(board)}")
-print(f"White bishop moves: {white_bishop_moves(board)}")
-print(f"Black bishop moves: {black_bishop_moves(board)}")
-'''
-
 print("/////////////////////////////////////////////////")
 print("Type 'new game' to begin a fresh game as white OR")
 fen_string = input("enter a FEN string to play from that position: ").strip().lower()
@@ -48,4 +32,5 @@ print("Goodbye!")
 
 
 
-"""Right now, illegal moves are able to be played. Checks, checkmate, enpassant and promotion are not implemented"""
+"""Right now, illegal moves are able to be played(step into or ignore checks).
+Checks, checkmate, enpassant and promotion are not implemented"""
